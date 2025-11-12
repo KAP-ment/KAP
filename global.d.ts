858012fdc2ddea62e967e087e4e7aa4ee13caba5
KAP-ment <kapmentt@gmail.com>
2025-11-12 21:27:33 +0530

    Create global.d.ts

---
+++ global.d.ts
@@ -0,0 +1,15 @@
+export {};
+
+declare global {
+  interface Window {
+    google?: {
+      accounts: {
+        id: {
+          initialize: (options: any) => void;
+          renderButton: (element: HTMLElement, options: any) => void;
+          prompt: () => void;
+        };
+      };
+    };
+  }
+}
